70ce6f3e0b7af0adb88a906c1044712070086adb
Alexander Fridman <AlexFridman@outlook.com>
2017-04-19 09:12:07 +0300

    Update .gitignore

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='visits-detector',
+    version='',
+    packages=['visits_detector'],
+    url='',
+    license='',
+    author='Alexander Fridman',
+    author_email='alexfridman@outlook.com',
+    description=''
+)
