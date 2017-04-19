44bdffdd679ed9182238cbbdade0e7b11b6ba28e
Alexander Fridman <alexfridman@outlook.com>
2017-04-19 15:55:13 +0300

    Remove extra fields from setup.py

--- setup.py
+++ setup.py
@@ -2,9 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='visits-detector',
-    version='',
     packages=find_packages(),
-    url='',
     install_requires=[
         'tensorflow',
         'keras',
@@ -13,8 +11,6 @@ setup(
         'pandas',
         'h5py'
     ],
-    license='',
     author='Alexander Fridman',
     author_email='alexfridman@outlook.com',
-    description=''
 )
